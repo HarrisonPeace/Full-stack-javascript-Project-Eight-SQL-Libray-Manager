a2cfb6a5260d44ab8532c73e5d0385b8e4b8da15
Harrison Peace <harrison@apwines.com>
2021-06-15 11:15:30 +1000

    re-formated code, fixed issue with no pagnation after book delete

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,29 +1,26 @@
-const Book = require('../models').Book;
-console.log(Book);
-
 function searchFunction() {
-  // Declare variables
-  let input, filter, table, tableRow, dataCaption;
-  input = document.getElementById('search');
-  filter = input.value.toUpperCase();
-  table = document.querySelector("tbody");
-  tableRow = table.getElementsByTagName('tr');
-  
-  // Loop through all list items, and hide those who don't match the search query
-  for (let i = 0; i < tableRow.length; i++) {
-    let filterItems = [];
-    let tableColumns = tableRow[i].querySelectorAll("td")
+	// Declare variables
+	let input, filter, table, tableRow, dataCaption;
+	input = document.getElementById('search');
+	filter = input.value.toUpperCase();
+	table = document.querySelector("tbody");
+	tableRow = table.getElementsByTagName('tr');
 
-    for (let j = 0; j < tableColumns.length; j++) {
-      filterItems.push(tableColumns[j].innerText)
-    }
+	// Loop through all list items, and hide those who don't match the search query
+	for (let i = 0; i < tableRow.length; i++) {
+		let filterItems = [];
+		let tableColumns = tableRow[i].querySelectorAll("td");
 
-    dataCaption = filterItems.join(' ');
-    
-    if (dataCaption.toUpperCase().indexOf(filter) > -1) {
-      tableRow[i].style.display = "";
-    } else {
-      tableRow[i].style.display = "none";
-    }
-  }
+		for (let j = 0; j < tableColumns.length; j++) {
+			filterItems.push(tableColumns[j].innerText);
+		}
+
+		dataCaption = filterItems.join(' ');
+
+		if (dataCaption.toUpperCase().indexOf(filter) > -1) {
+			tableRow[i].style.display = "";
+		} else {
+			tableRow[i].style.display = "none";
+		}
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+const itemsPerPage = 10;
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -14,6 +15,14 @@ function asyncHandler(cb){
   }
 }
 
+function createPagination(page, itemsPerPage, books, res, deletedBook = null) {
+  let totalPages = Math.ceil(books.length / itemsPerPage);
+  let startIndex = (page * itemsPerPage) - itemsPerPage; //first list item to be shown on page
+  let endIndex = (page * itemsPerPage - 1); //last list item to be shown on page
+  const filteredBooks = books.filter((book, index) => index >= startIndex && index <= endIndex);
+  res.render('index', { books: filteredBooks, title: 'SQL Library Manager', pages: totalPages, page, deletedBook })
+}
+
 /* Redirect to page 1 */
 router.get('/', function(req, res, next) {
   res.redirect('/1');
@@ -22,29 +31,26 @@ router.get('/', function(req, res, next) {
 /* GET home page. */
 router.get('/:page', asyncHandler(async (req, res, next) => {
   const page = req.params.page;
-  const itemsPerPage = 10;
   const books = await Book.findAll({ order: [[ "year", "DESC" ]] })
   if (books.length >= 1) {
-    if (books.length <= 10) {
-    res.render('index', { books, title: 'SQL Library Manager' })
-    } else {
-      let totalPages = Math.ceil(books.length / itemsPerPage);
-      console.log(totalPages);
-      let startIndex = (page * itemsPerPage) - itemsPerPage; //first list item to be shown on page
-      let endIndex = (page * itemsPerPage - 1); //last list item to be shown on page
-      const filteredBooks = books.filter((book, index) => index >= startIndex && index <= endIndex);
-      res.render('index', { books: filteredBooks, title: 'SQL Library Manager', pages: totalPages, page })
-    }
+    books.length <= 10
+    ? res.render('index', { books, title: 'SQL Library Manager' })
+    : createPagination(page, itemsPerPage, books, res);
   } else {
-    res.render('index', { books: {}, title: 'SQL Library Manager',  noBooks: true })
+    res.render('index', { books: {}, title: 'SQL Library Manager',  noBooks: true });
   }
 }));
 
 router.get('/deleted/:book', asyncHandler(async (req, res, next) => {
+  const page = 1;
   const books = await Book.findAll({ order: [[ "year", "DESC" ]] })
-  books.length >= 1
-  ? res.render('index', { books, title: 'SQL Library Manager', deletedBook: req.params.book })
-  : res.render('index', { books: {}, title: 'SQL Library Manager',  noBooks: true, deletedBook: req.params.book })
+  if (books.length >= 1) {
+    books.length <= 10
+    ? res.render('index', { books, title: 'SQL Library Manager', deletedBook: req.params.book })
+    : createPagination(page, itemsPerPage, books, res, req.params.book);
+  } else {
+    res.render('index', { books: {}, title: 'SQL Library Manager',  noBooks: true, deletedBook: req.params.book });
+  }
 }));
 
 module.exports = router;
